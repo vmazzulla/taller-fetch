642849b9875da2b189a5384deda8b9c5c4106846
vmazzulla <vmazzulla12@gmail.com>
2024-08-23 19:47:12 -0300

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -11,11 +11,19 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
 function showData(dataArray) {
-  // El for itera sobre los elementos del array
-  for (const item of dataArray) {
-    // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
-    container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
-  }
+
+    // El for itera sobre los elementos del array
+    for (const item of dataArray) {
+        // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
+        container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
+    }
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL)
+    .then(response=> response.json())
+    .then(data=>{
+
+        showData(data.students);
+    });
